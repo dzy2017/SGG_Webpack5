6fff079aef8af09a4353218c9cc7b54959b0e4ba
DaiZhengYang <10981505+DaiZhengYang@user.noreply.gitee.com>
2022-05-31 04:03:20 +0800

    first commit

---
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -0,0 +1,16 @@
+export const siteData = {
+  "base": "/",
+  "lang": "zh-CN",
+  "title": "尚硅谷 Web 前端之 Webpack5 教程",
+  "description": "Webpack5从小白到大神",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/imgs/favicon.ico"
+      }
+    ]
+  ],
+  "locales": {}
+}
